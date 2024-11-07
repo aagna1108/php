56760da6b226e329f3bb1fc590ee17c2207d59ce
aagna1108 <166470744+aagna1108@users.noreply.github.com>
2024-11-07 18:49:19 +0530

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,10 @@
+<?php
+$host="localhost";
+$user="root";
+$password="";
+$db="login";
+$conn=new mysqli($host,$user,$password,$db);
+if($conn->connect_error){
+    echo "Failed to connect DB".$conn->connect_error;
+}
+?>
---
+++ footer.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</head>
+<body>
+    <footer class="p-5 bg-body-secondary"> 
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-sm-3 text-black mb-1 border border-0 border-dark">
+                    <img src="image/logo.png" alt="jewellery" height="auto" width="80px"> Jewellery Shop
+                    <p>Jewelry refers to an adornment (generally precious metal or stone) worn by a person, like a bracelet, ring, necklace, or earrings. Jewelry may be purely decorative, or it may express something — like a ring that says you are married.</p>
+                    <div class="container text-left">
+                        <i class="bi bi-instagram"></i>
+                    <i class="bi bi-facebook"></i>
+                    </div>
+                </div>
+                <div class="col-sm-3 text-black border border-0 border-dark">
+                    <h3 class="pb-4">My Account</h3>
+                    <p>Home</p>
+                    <p>About Us</p>
+                    <p>Shop</p>
+                    <p>Contact Us</p>
+                </div>
+                <div class="col-sm-3 text-black border border-0 border-dark">
+                    <h3 class="pb-4">About Us</h3>
+                    <p>Order Tracking</p>
+                    <p>My Account</p>
+                    <p>Checkout</p>
+                    <p>Wishlist</p>
+                </div>
+                <div class="col-sm-3 text-black border border-0 border-dark">
+                    <h3 class="pb-4">Newsletter</h3>
+                    <form>
+                        <div class="mb-3">
+                          <label class="form-label">Name</label>
+                          <input type="password" class="form-control" id="password">
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">Email address</label>
+                            <input type="email" class="form-control" id="username">
+                          </div>
+                        <button type="button" class="btn btn-dark" onclick="login()">Submit</button>
+                      </form>
+                </div>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
