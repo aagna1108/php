c5e25f15c2646926e54527791aafecee61533963
aagna1108 <166470744+aagna1108@users.noreply.github.com>
2024-11-09 18:43:05 +0530

    Update index.php

--- index.php
+++ index.php
@@ -13,12 +13,12 @@
         <form method="post" action="register.php">
             <div class="input-group">
                 <i class="fas fa-user"></i>
-                <input type="text" name="fName" id="fName" placeholder="First Name" required>
+                <input type="text" name="firstname" id="fName" placeholder="First Name" required>
                 <label for="fname">First Name</label>
             </div>
             <div class="input-group">
                 <i class="fas fa-user"></i>
-                <input type="text" name="lName" id="lName" placeholder="Last Name" required>
+                <input type="text" name="lastname" id="lName" placeholder="Last Name" required>
                 <label for="lname">Last Name</label>
             </div>
             <div class="input-group">
@@ -31,14 +31,15 @@
                 <input type="password" name="password" id="password" placeholder="password" required>
                 <label for="Password">Password</label>
             </div>
-            <input type="Submit" class="btn" value="Sign Up" name="Sign Up">
+            <input type="Submit" class="btn" value="Sign Up" name="signUp">
         </form>
         <p class="or">
             -------or------
         </p>
         <div class="icons">
-            <i class="fab fa-google"></i>
-            <i class="fab fa-facebook"></i>
+            <a href="https://www.google.co.in/"><i class="fab fa-google"></i></a>
+            
+            <a href="https://www.facebook.com/"><i class="fab fa-facebook"></i></a>
         </div>
         <div class="links">
             <p>Already Have Account?</p>
@@ -49,7 +50,6 @@
     <div class="container" id="signIn">
         <h1 class="form-title">Sign In</h1>
         <form method="post" action="register.php">
-            
             <div class="input-group">
                 <i class="fas fa-envelope"></i>
                 <input type="email" name="email" id="email" placeholder="Email" required>
@@ -57,20 +57,20 @@
             </div>
             <div class="input-group">
                 <i class="fas fa-lock"></i>
-                <input type="password" id="password" placeholder="password" required>
+                <input type="password" name="password" id="password" placeholder="password" required>
                 <label for="Password">Password</label>
             </div>
             <p class="recover">
                 <a href="#">Recover Password</a>
             </p>
-            <input type="Submit" class="btn" value="Sign In" name="Sign In">
+            <input type="Submit" class="btn" value="Sign In" name="signIn">
         </form>
         <p class="or">
             -------or------
         </p>
         <div class="icons">
-            <i class="fab fa-google"></i>
-            <i class="fab fa-facebook"></i>
+        <a href="https://www.google.co.in/"><i class="fab fa-google"></i></a>
+        <a href="https://www.facebook.com/"><i class="fab fa-facebook"></i></a>
         </div>
         <div class="links">
             <p>Don't have account yet?</p>
